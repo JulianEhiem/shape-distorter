795a4bbcc403b035b779f5430ab844221306d923
Julian_Ehiem <jules.ehiem@gmail.com>
2020-07-14 21:53:27 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,4 @@
+const topLeft = document.querySelector("#top-left").value;
+const topRight = document.querySelector("#top-right").value;
+const bottomRight = document.querySelector("#bottom-right").value;
+const bottomLeft = document.querySelector("#bottom-left").value;
